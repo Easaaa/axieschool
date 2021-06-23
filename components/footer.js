eb9ba73da909cfcfd439655438ccb3b579c2d39c
Leonardo T <easaaa@protonamail.com>
2021-06-23 16:32:59 +0200

    Fixed invited link

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
           </h3>
           <div className='flex flex-col items-center justify-center lg:flex-row lg:pl-4 lg:w-1/2'>
             <a
-              href='https://discord.gg/73NPjpX4'
+              href='https://discord.gg/NsUavgczyP'
               className='px-12 py-3 mx-3 mb-6 font-bold text-white transition-colors duration-200 bg-black border border-black hover:bg-white hover:text-black lg:px-8 lg:mb-0'>
               Join our Discord
             </a>
