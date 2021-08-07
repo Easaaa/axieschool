473586ef49a9be5b7941ea167c69f6e1f6f1bf4f
Leonardo T <easaaa@protonamail.com>
2021-08-07 20:56:54 +0200

    Fix color white mode

--- pages/index.js
+++ pages/index.js
@@ -11,35 +11,35 @@ export default function Index() {
         </Head>
         <Container>
           <section class='text-gray-600 body-font'>
-            <div class='mx-auto flex px-5 py-36 pb-60 items-center justify-center flex-col'>
+            <div class='mx-auto flex py-10 md:py-36 pb-60 items-center justify-center flex-col'>
               <img
                 class='lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded'
                 alt='hero'
                 src='/logo.png'
               />
               <div class='text-center lg:w-2/3 w-full -mt-8'>
-                <h1 class='font-bold leading-tight tracking-tighter sm:text-5xl text-3xl mb-4 text-white antialiased'>
+                <h1 class='font-bold leading-tight tracking-tighter sm:text-5xl text-3xl mb-4 text-gray-800 dark:text-white antialiased'>
                   We are Axie School
                 </h1>
-                <p class='mb-8 leading-relaxed text-gray-200'>
+                <p class='mb-8 leading-relaxed text-gray-700 dark:text-gray-200'>
                   Join our growing community of Axies lovers. We are building
                   strong teams ready to challenge the entire axie world. Every
                   week we have some slot available for scholarship. Join the
                   server, and compile the form if you want be part of this
                   journey with us!
                 </p>
-                <div class='flex justify-center'>
+                <div class='grid md:grid-cols-2 place-items-center justify-center gap-4 md:gap-6 w-full'>
                   <a
                     target='_blank'
                     href='https://discord.gg/4RCFsVMeMw'
-                    class='inline-flex text-gray-900 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-gray-100 rounded text-lg'>
+                    class='w-full dark:text-gray-900 dark:bg-white text-gray-800 bg-gray-300 border-0 py-2 px-6 focus:outline-none hover:bg-gray-100 rounded text-lg'>
                     Join Discord
                   </a>
                   <a
                     target='_blank'
                     href='https://docs.google.com/forms/d/e/1FAIpQLSeNtBs0mG4xD97RZGJSCMHMII0BaCu1baL_y3Wq6jqB06kzTA/viewform'
                     style={{ backgroundColor: '#e94661' }}
-                    class='ml-4 inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg'>
+                    class='text-white border-0 py-2 px-6 focus:outline-none rounded text-lg'>
                     Fill the Form
                   </a>
                 </div>
