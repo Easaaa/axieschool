4783f56a46797caeb023c26062bc1c197025f1c2
Leonardo T <easaaa@protonamail.com>
2021-08-07 21:01:39 +0200

    Fix buttons size

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Index() {
         </Head>
         <Container>
           <section class='text-gray-600 body-font'>
-            <div class='mx-auto flex py-10 md:py-36 pb-60 items-center justify-center flex-col'>
+            <div class='mx-auto flex px-5 py-10 md:py-36 pb-60 md:-mt-20 items-center justify-center flex-col'>
               <img
                 class='lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded'
                 alt='hero'
                 src='/logo.png'
               />
-              <div class='text-center lg:w-2/3 w-full -mt-8'>
+              <div class='text-center lg:w-2/3 w-full -mt-8 md:px-24'>
                 <h1 class='font-bold leading-tight tracking-tighter sm:text-5xl text-3xl mb-4 text-gray-800 dark:text-white antialiased'>
                   We are Axie School
                 </h1>
@@ -28,18 +28,18 @@ export default function Index() {
                   server, and compile the form if you want be part of this
                   journey with us!
                 </p>
-                <div class='grid md:grid-cols-2 place-items-center justify-center gap-4 md:gap-6 w-full'>
+                <div class='grid md:grid-cols-2 place-items-center place-content-center gap-4 md:gap-6 max-w-sm mx-auto'>
                   <a
                     target='_blank'
                     href='https://discord.gg/4RCFsVMeMw'
-                    class='w-full dark:text-gray-900 dark:bg-white text-gray-800 bg-gray-300 border-0 py-2 px-6 focus:outline-none hover:bg-gray-100 rounded text-lg'>
+                    class='flex place-content-center dark:text-gray-900 text-center w-80 md:w-40 dark:bg-white text-gray-800 bg-gray-300 border-0 py-2 px-6 focus:outline-none hover:bg-gray-100 rounded text-lg'>
                     Join Discord
                   </a>
                   <a
                     target='_blank'
                     href='https://docs.google.com/forms/d/e/1FAIpQLSeNtBs0mG4xD97RZGJSCMHMII0BaCu1baL_y3Wq6jqB06kzTA/viewform'
                     style={{ backgroundColor: '#e94661' }}
-                    class='text-white border-0 py-2 px-6 focus:outline-none rounded text-lg'>
+                    class='flex place-content-center text-white border-0 w-80 text-center py-2 px-6 md:w-40 focus:outline-none rounded text-lg'>
                     Fill the Form
                   </a>
                 </div>
